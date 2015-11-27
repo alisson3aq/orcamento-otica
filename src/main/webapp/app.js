146e5b8c42400dc39c81b73feb691d0fa63c6ee8
mgustavo <mgustavo@ciandt.com>
2015-11-27 16:37:29 -0200

    Primeiro Commit

---
+++ src/main/webapp/app.js
@@ -0,0 +1,41 @@
+var mainApp = angular.module("mainApp", [ 'ngRoute' ]);
+
+mainApp
+.controller('studentController', function($scope) {
+	$scope.printDiv = function(divName) {
+		  var printContents = document.getElementById(divName).innerHTML;
+		  var popupWin = window.open('', '_blank');
+		  popupWin.document.open()
+		  popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + printContents + '</html>');
+		  popupWin.document.close();
+		} 
+})
+.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/clientes', {
+		templateUrl : 'views/clientes.html',
+		controller : 'studentController'
+	}).when('/medicos', {
+		templateUrl : 'views/medicos.html',
+		controller : 'studentController'
+	}).when('/orcamentos', {
+		templateUrl : 'views/orcamentos.html',
+		controller : 'studentController'
+	}).when('/produtos', {
+		templateUrl : 'views/produtos.html',
+		controller : 'studentController'
+	}).when('/servicos', {
+		templateUrl : 'views/servicos.html',
+		controller : 'studentController'
+	}).when('/servicos', {
+		templateUrl : 'views/servicos.html',
+		controller : 'studentController'
+	}).when('/usuarios', {
+		templateUrl : 'views/usuarios.html',
+		controller : 'studentController'
+	}).when('/empresa', {
+		templateUrl : 'views/empresa.html',
+		controller : 'studentController'
+	}).otherwise({
+		redirectTo : '/orcamentos'
+	});
+} ]);
