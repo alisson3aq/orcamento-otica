1e2ea442d1a318c6b577d6277d9c1a330cdc4094
mgustavo <mgustavo@ciandt.com>
2015-11-30 16:02:21 -0200

    Evoluindo relatorio

--- src/main/java/orcamento/servlet/ReportServlet.java
+++ src/main/java/orcamento/servlet/ReportServlet.java
@@ -1,11 +1,14 @@
 package orcamento.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collection;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -14,38 +17,34 @@ import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
-public class RepportServlet extends HttpServlet {
+public class ReportServlet extends HttpServlet {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
-		JasperReport report = null;
+
+
 		try {
-			report = JasperCompileManager.compileReport("/home/mgustavo/dev/EclipseProjects/orcamento/src/main/resources/jasper/orcamento.jrxml");
+			System.out.println("1");
+			JasperReport report = JasperCompileManager.compileReport("/home/mgustavo/dev/EclipseProjects/orcamento/src/main/resources/jasper/orcamento.jrxml");
+			System.out.println("2");
+			Collection<?> clientes = null;
+			JasperPrint print = null;
+			print = JasperFillManager.fillReport(report, null,new JRBeanCollectionDataSource(clientes));
+			System.out.println("3");
+			
+			//JasperExportManager.exportReportToPdfFile(print,"/home/mgustavo/Relatorio_de_Clientes.pdf");
+			
+			
+			resp.getOutputStream().write(JasperExportManager.exportReportToPdf(print));
+			
+			System.out.println("4");
 		} catch (JRException e) {
 			e.printStackTrace();
 		}
-		
-		Collection<?> clientes = null;
-		JasperPrint print = null;
-		try {
-			print = JasperFillManager.fillReport(report, null, new JRBeanCollectionDataSource(clientes));
-		} catch (JRException e) {
-			e.printStackTrace();
-		}
- 
-		try {
-			JasperExportManager.exportReportToPdfFile(print, "/home/mgustavo/Relatorio_de_Clientes.pdf");
-		} catch (JRException e) {
-			e.printStackTrace();
-		}		
-	
+
 	}
-	
+
 }
