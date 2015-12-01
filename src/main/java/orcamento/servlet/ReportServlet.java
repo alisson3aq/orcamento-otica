652ba6c6792f6249a19dca1e1b0e6d46ebf6b9d6
mgustavocoder <mgustavocoder@gmail.com>
2015-11-30 22:16:03 -0200

    corrigindo path absoluto para jasper

--- src/main/java/orcamento/servlet/ReportServlet.java
+++ src/main/java/orcamento/servlet/ReportServlet.java
@@ -1,44 +1,78 @@
 package orcamento.servlet;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.xml.JasperDesignFactory;
 
 public class ReportServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		try {
-			System.out.println("Diretorio do jasper: " + getServletContext().getRealPath("../resources/jasper/orcamento.jrxml"));
+			Map paramMap = new HashMap();
+			/*	InputStream reportStream = ClassLoader.getSystemResourceAsStream("/home/mgustavocoder/dev/eclipse-workspace/orcamento/src/main/resources/jasper/orcamento.jasper");
+			ServletOutputStream servletOutputStream = resp.getOutputStream();
+			resp.setContentType("application/pdf");
+			JasperRunManager.runReportToPdfStream(reportStream, servletOutputStream, parameterMap);
+*/
+
+
+			//byte[] byteStream = JasperRunManager.runReportToPdf("/home/mgustavocoder/dev/eclipse-workspace/orcamento/src/main/resources/jasper/orcamento.jasper",paramMap);
+			byte[] byteStream = JasperRunManager.runReportToPdf("/var/lib/openshift/56589fd589f5cfbf2e000042/app-root/repo/target/classes/jasper/orcamento.jasper",paramMap);
+		
+			//resp.setHeader("Content-Disposition","inline, filename=myReport.pdf");
+			resp.setContentType("application/pdf");
+			resp.setContentLength(byteStream.length);
+			resp.getOutputStream().write(byteStream,0,byteStream.length);
+			resp.getOutputStream().flush();     
+			resp.getOutputStream().close(); 
 			
-			JasperReport report = JasperCompileManager.compileReport("/var/lib/openshift/56589fd589f5cfbf2e000042/app-root/repo/target/classes/jasper/orcamento.jrxml");
-			System.out.println("2");
-			Collection<?> clientes = null;
-			JasperPrint print = null;
-			print = JasperFillManager.fillReport(report, null,new JRBeanCollectionDataSource(clientes));
-			System.out.println("3");
-			
-			//JasperExportManager.exportReportToPdfFile(print,"/home/mgustavo/Relatorio_de_Clientes.pdf");
-			
-			
-			resp.getOutputStream().write(JasperExportManager.exportReportToPdf(print));
-			
-			System.out.println("4");
+			// System.out.println("Diretorio do jasper: " +
+			// getServletContext().getRealPath("../resources/jasper/orcamento.jrxml"));
+
+			// JasperReport report =
+			// JasperCompileManager.compileReport("/var/lib/openshift/56589fd589f5cfbf2e000042/app-root/repo/target/classes/jasper/orcamento.jrxml");
+			// JasperReport report =
+			// JasperCompileManager.compileReport(getServletContext().getRealPath("../resources/jasper/orcamento.jrxml"));
+
+			/*
+			 * System.out.println("2"); Collection<?> clientes = null;
+			 * JasperPrint print = null; print =
+			 * JasperFillManager.fillReport(report, null,new
+			 * JRBeanCollectionDataSource(clientes)); System.out.println("3");
+			 */
+			// JasperExportManager.exportReportToPdfFile(print,"/home/mgustavo/Relatorio_de_Clientes.pdf");
+
+			// resp.getOutputStream().write(JasperExportManager.exportReportToPdf(print));
+
+			// JasperRunManager.runReportToPdfStream(print,
+			// resp.getOutputStream(), null, new JREmptyDataSource());
+
+			//System.out.println("4");
 		} catch (JRException e) {
 			e.printStackTrace();
 		}
