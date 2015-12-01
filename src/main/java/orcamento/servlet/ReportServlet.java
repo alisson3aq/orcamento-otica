e467f8abead524b3600f77593b819d1babbc5144
mgustavocoder <mgustavocoder@gmail.com>
2015-11-30 22:25:46 -0200

    corrigindo path absoluto para jasper

--- src/main/java/orcamento/servlet/ReportServlet.java
+++ src/main/java/orcamento/servlet/ReportServlet.java
@@ -23,6 +23,7 @@ import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.engine.xml.JasperDesignFactory;
 
 public class ReportServlet extends HttpServlet {
@@ -32,24 +33,54 @@ public class ReportServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		try {
+		//try {
 			Map paramMap = new HashMap();
 			/*	InputStream reportStream = ClassLoader.getSystemResourceAsStream("/home/mgustavocoder/dev/eclipse-workspace/orcamento/src/main/resources/jasper/orcamento.jasper");
 			ServletOutputStream servletOutputStream = resp.getOutputStream();
 			resp.setContentType("application/pdf");
 			JasperRunManager.runReportToPdfStream(reportStream, servletOutputStream, parameterMap);
 */
+			 //Variaveis locais..  
+	        JasperReport jasperReport = null;  
+	        byte[] pdfSegundaVia = null;  
+			
+			 //Carrega o arquivo com o jasperReport  
+	        try {  
+	            jasperReport = (JasperReport) JRLoader.loadObject( "/var/lib/openshift/56589fd589f5cfbf2e000042/app-root/repo/target/classes/jasper/orcamento.jasper");  
+	        } catch (JRException jre) {  
+	            jre.printStackTrace();  
+	        }  
+	        
+	      //Gera o pdf para exibicao..  
+	        try {  
+	            pdfSegundaVia = JasperRunManager.runReportToPdf( jasperReport, paramMap , new JREmptyDataSource() );  
+	        } catch (JRException jre) {  
+	            jre.printStackTrace();  
+	        }  
+	          
+	        //Parametros para nao fazer cache e o que será exibido..  
+	        resp.setContentType( "application/pdf" );  
+	        resp.setHeader( "Cache-Control", "no-store" );  
+	        resp.setHeader( "Pragma", "no-cache" );  
+	        resp.setDateHeader( "Expires", 0 );  
+	          
+	        //Envia para o navegador o pdf..  
+	        ServletOutputStream servletOutputStream = resp.getOutputStream();  
+	        servletOutputStream.write( pdfSegundaVia );  
+	        servletOutputStream.flush();  
+	        servletOutputStream.close();  
+			
 
 
 			//byte[] byteStream = JasperRunManager.runReportToPdf("/home/mgustavocoder/dev/eclipse-workspace/orcamento/src/main/resources/jasper/orcamento.jasper",paramMap);
-			byte[] byteStream = JasperRunManager.runReportToPdf("/var/lib/openshift/56589fd589f5cfbf2e000042/app-root/repo/target/classes/jasper/orcamento.jasper",paramMap);
+		/*	byte[] byteStream = JasperRunManager.runReportToPdf("/var/lib/openshift/56589fd589f5cfbf2e000042/app-root/repo/target/classes/jasper/orcamento.jasper",paramMap);
 		
 			//resp.setHeader("Content-Disposition","inline, filename=myReport.pdf");
 			resp.setContentType("application/pdf");
 			resp.setContentLength(byteStream.length);
 			resp.getOutputStream().write(byteStream,0,byteStream.length);
 			resp.getOutputStream().flush();     
-			resp.getOutputStream().close(); 
+			resp.getOutputStream().close(); */
 			
 			// System.out.println("Diretorio do jasper: " +
 			// getServletContext().getRealPath("../resources/jasper/orcamento.jrxml"));
@@ -73,9 +104,9 @@ public class ReportServlet extends HttpServlet {
 			// resp.getOutputStream(), null, new JREmptyDataSource());
 
 			//System.out.println("4");
-		} catch (JRException e) {
+/*		} catch (JRException e) {
 			e.printStackTrace();
-		}
+		}*/
 
 	}
 
