10ceced10493719a45ec558eecba3f782b91f971
mgustavo <mgustavo@ciandt.com>
2015-12-03 16:52:56 -0200

    Separando controllers por arquivos

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,45 +1,31 @@
 var mainApp = angular.module("mainApp", [ 'ngRoute' ]);
 
 mainApp
-.controller('studentController', function($scope) {
-	$scope.printDiv = function(divName) {
-		  var printContents = document.getElementById(divName).innerHTML;
-		  var popupWin = window.open('', '_blank');
-		  popupWin.document.open()
-		  popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + printContents + '</html>');
-		  popupWin.document.close();
-		}
-
-	$scope.exportToPDF = function(){
-		window.open("/reportServlet", "_blank");
-	}	
- 
-})
 .config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/clientes', {
 		templateUrl : 'views/clientes.html',
-		controller : 'studentController'
+		controller : 'navController'
 	}).when('/medicos', {
 		templateUrl : 'views/medicos.html',
-		controller : 'studentController'
+		controller : 'navController'
 	}).when('/orcamentos', {
 		templateUrl : 'views/orcamentos.html',
-		controller : 'studentController'
+		controller : 'navController'
 	}).when('/produtos', {
 		templateUrl : 'views/produtos.html',
-		controller : 'studentController'
+		controller : 'navController'
 	}).when('/servicos', {
 		templateUrl : 'views/servicos.html',
-		controller : 'studentController'
+		controller : 'navController'
 	}).when('/servicos', {
 		templateUrl : 'views/servicos.html',
-		controller : 'studentController'
+		controller : 'navController'
 	}).when('/usuarios', {
 		templateUrl : 'views/usuarios.html',
-		controller : 'studentController'
+		controller : 'navController'
 	}).when('/empresa', {
 		templateUrl : 'views/empresa.html',
-		controller : 'studentController'
+		controller : 'navController'
 	}).otherwise({
 		redirectTo : '/orcamentos'
 	});
