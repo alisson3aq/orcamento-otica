8fc3b937d422da4b5a5e47daa75d3661c6a38f5e
mgustavocoder <mgustavocoder@gmail.com>
2015-12-03 20:21:56 -0200

    adicionando controller no modal

---
+++ src/main/webapp/app/controllers/orcamentoCrtl.js
@@ -0,0 +1,14 @@
+mainApp
+.controller('orcamentoCrtl', function($scope) {
+    $scope.printDiv = function(divName) {
+          var printContents = document.getElementById(divName).innerHTML;
+          var popupWin = window.open('', '_blank');
+          popupWin.document.open()
+          popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + printContents + '</html>');
+          popupWin.document.close();
+        }
+
+    $scope.exportToPDF = function(){
+        window.open("/reportServlet", "_blank");
+    }
+})
