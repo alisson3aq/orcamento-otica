8c9a0e358a6b1822dc8941147815ec2c3419e85d
mgustavo <mgustavo@ciandt.com>
2015-12-03 17:15:55 -0200

    [bugfix]

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,6 +1,19 @@
 var mainApp = angular.module("mainApp", [ 'ngRoute' ]);
 
 mainApp
+.controller('orcamentoCrtl', function($scope) {
+    $scope.printDiv = function(divName) {
+          var printContents = document.getElementById(divName).innerHTML;
+          var popupWin = window.open('', '_blank');
+          popupWin.document.open()
+          popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + printContents + '</html>');
+          popupWin.document.close();
+        }
+
+    $scope.exportToPDF = function(){
+        window.open("/reportServlet", "_blank");
+    }
+})
 .config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/clientes', {
 		templateUrl : 'views/clientes.html',
