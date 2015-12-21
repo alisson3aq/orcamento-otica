4d60940efdfdde9021335a337e464e9c8c9219a0
mgustavo <mgustavo@ciandt.com>
2015-12-21 15:40:53 -0200

    instalando grunt para minify

---
+++ src/main/webapp/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function( grunt ) {
+
+  grunt.initConfig({
+
+    uglify : {
+      options : {
+        mangle : false
+      },
+
+      my_target : {
+        files : {
+          'app/app_minified.js' : 
+		[ 'app/app.js',
+		'app/controllers/clientesController.js',  
+		'app/controllers/empresaController.js',  
+		'app/controllers/orcamentoController.js',  
+		'app/controllers/produtosController.js',  
+		'app/controllers/servicosController.js',  			
+		'app/controllers/usuariosController.js'
+		]
+        }
+      }
+    } // uglify
+
+  });
+
+
+  // Plugins do Grunt
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+
+
+  // Tarefas que serão executadas
+  grunt.registerTask( 'default', [ 'uglify' ] );
+
+};
