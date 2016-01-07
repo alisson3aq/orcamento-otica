063c3101b9e2e1915d77f92408eda1c5fa4c622f
mgustavo <mgustavo@ciandt.com>
2016-01-07 18:27:11 -0200

    adicionando filtro para os orcamentos

--- src/main/java/orcamento/dao/OrcamentosDAO.java
+++ src/main/java/orcamento/dao/OrcamentosDAO.java
@@ -5,7 +5,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -41,6 +44,38 @@ public class OrcamentosDAO {
         EntityManager manager = factory.createEntityManager();
 
         Query query = manager.createQuery("SELECT s FROM OrcamentoBean s" ) ;
+        List<OrcamentoBean> listOrcamento = query.getResultList();
+
+        factory.close();
+        return listOrcamento;
+    }
+
+
+    public Collection<OrcamentoBean> listOrcamentos(Date from, Date to){
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("orcamento_pu");
+        EntityManager manager = factory.createEntityManager();
+
+        from = new Date();
+        to = new Date();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(from);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        from =  calendar.getTime();
+
+        calendar.setTime(to);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        to =  calendar.getTime();
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String frmDate = format.format(from);
+        String enDate = format.format(to);
+
+        Query query = manager.createQuery("SELECT s FROM OrcamentoBean s WHERE s.dataorcamento BETWEEN '" + frmDate  + "' AND '" + enDate + "'");
         List<OrcamentoBean> listOrcamento = query.getResultList();
 
         factory.close();
